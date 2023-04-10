b4e211663aebe76b023ed51ccd53b65fbbd917b1
crz29 <crz29@hotmail.com>
2023-04-09 23:22:04 -0300

    Subiendo carga de tarea terminada

---
+++ app.js
@@ -0,0 +1,23 @@
+const fecha = document.querySelector('#fecha');
+const input = document.querySelector('#input');
+const botonEnter = document.querySelector('#enter');
+const lista = document.querySelector('#lista');
+
+//funcion agregar tarea
+function agregarTarea(tarea){
+    const elemento =`<li id="elemento">
+    <i class="fa-sharp fa-solid fa-circle-check" data="realizado" id="0"></i>
+    <p class="text">${tarea}</p>
+    <i class="fa-solid fa-circle-minus" data="eliminado" id="0"></i><!--identificador data-->
+</li>  `
+
+    lista.insertAdjacentHTML('beforeend',elemento);
+}
+
+botonEnter.addEventListener('click', () =>{//le paso una funcion arrow
+    const tarea = input.value;
+    if(tarea){
+        agregarTarea(tarea);
+    }else{alert("Ingresa una tarea")};
+    input.value ='';
+})
